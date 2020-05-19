dc3afb9821bd89757103233a879f80e940911e7a
yangyunhui454545 <yyang7412@gmail.com>
2020-05-19 14:55:56 +0900

    hello

--- src/main/java/com/tave/project/ProjectApplication.java
+++ src/main/java/com/tave/project/ProjectApplication.java
@@ -2,12 +2,25 @@ package com.tave.project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
-@SpringBootApplication
+
+@Configuration
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class ProjectApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ProjectApplication.class, args);
     }
 
+    @Bean
+    public InternalResourceViewResolver setupViewResolver(){
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("/WEB-INF/view/");
+        resolver.setSuffix(".jsp");
+        return resolver;
+    }
 }
